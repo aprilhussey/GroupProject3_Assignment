7f25d32025df473a1eb99c4d1bb2e38604ecc09e
HollyAnna2002 <hollyhumphries2002@gmail.com>
2023-10-31 12:32:57 +0000

    Paladin Special Attack
    Paladin Special Attack is largely done, setup for other special attacks in code is ready to go

--- Goblin Gauntlet/Assets/Scripts/playerAttack.cs
+++ Goblin Gauntlet/Assets/Scripts/playerAttack.cs
@@ -9,11 +9,17 @@ public class playerAttack : MonoBehaviour
     public BoxCollider rogueAttackCollider;
     public GameObject warlockCastShot;
     public BoxCollider clericAttackCollider;
+    public GameObject paladinBeam;
     float attackTime;
     private float paladinAttackTime = 0.9f;
     private float rogueAttackTime = 0.2f;
     private float warlockAttackTime = 0.5f;
     private float clericAttackTime = 0.7f;
+    private float paladinSpecialCooldown = 1200;
+    private float rogueSpecialCooldown = 600;
+    private float warlockSpecialCooldown = 900;
+    private float clericSpecialCooldown = 600;
+    float specialCooldown;
     public bool attacking = false;
     private Rigidbody rb;
     public float classID = 0;
@@ -26,27 +32,61 @@ public class playerAttack : MonoBehaviour
             case 0:
                 attackCollider = paladinAttackCollider;
                 attackTime = paladinAttackTime;
-                return;
+                specialCooldown = paladinSpecialCooldown;
+                break;
             case 1:
                 attackCollider = rogueAttackCollider;
                 attackTime = rogueAttackTime;
-                return;
+                specialCooldown = rogueSpecialCooldown;
+                break;
             case 2:
                 attackCollider = paladinAttackCollider;
                 attackTime = warlockAttackTime;
-                return;
+                specialCooldown = warlockSpecialCooldown;
+                break;
             case 3:
                 attackCollider = clericAttackCollider;
                 attackTime = clericAttackTime;
-                return;
+                specialCooldown = clericSpecialCooldown;
+                break;
         }
         attackCollider.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (specialCooldown > 0)
+        {
+            specialCooldown -= 1;
+        }
     }
     void OnAttack()
     {
         if (!attacking)
         {
             StartCoroutine(AttackCooldown());
+        }
+    }
+
+    void OnSpecialAttack()
+    {
+        if (specialCooldown == 0)
+        {
+            switch (classID)
+            {
+                case 0:
+                    StartCoroutine(PaladinSpecialAttack());
+                    break;
+                case 1:
+                    StartCoroutine(RogueSpecialAttack());
+                    break;
+                case 2:
+                    StartCoroutine(WarlockSpecialAttack());
+                    break;
+                case 3:
+                    StartCoroutine(ClericSpecialAttack());
+                    break;
+            }
         }
     }
 
@@ -76,5 +116,32 @@ public class playerAttack : MonoBehaviour
         attackCollider.enabled = false;
         attacking = false;
         print("attack done!");
+    }
+
+    IEnumerator PaladinSpecialAttack()
+    {
+        print("special attacking");
+        specialCooldown = 1200;
+        GameObject childObject = Instantiate(paladinBeam) as GameObject;
+        childObject.transform.parent = this.transform;
+        childObject.transform.position = this.transform.position + new Vector3(0, 1.5f, 10.5f);
+        yield return new WaitForSeconds(3);
+        Destroy(childObject);
+        print("special attack done");
+    }
+
+    IEnumerator RogueSpecialAttack()
+    {
+        yield return new WaitForSeconds(5);
+    }
+
+    IEnumerator WarlockSpecialAttack()
+    {
+        yield return new WaitForSeconds(5);
+    }
+
+    IEnumerator ClericSpecialAttack()
+    {
+        yield return new WaitForSeconds(5);
     }
 }
