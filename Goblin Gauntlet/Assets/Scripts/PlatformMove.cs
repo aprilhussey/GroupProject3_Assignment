9000700f01b0426fe2163eab5a71038b940c0899
HollyAnna2002 <101565640+HollyAnna2002@users.noreply.github.com>
2023-11-21 17:09:05 +0000

    Beginning platform movement
    Working on basic movement for the platform, using turn points and tags to change its direction throughout the level. Untested and no support for barricades yet.

---
+++ Goblin Gauntlet/Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    float speed = 1f;
+    Vector3 moveDirection;
+    Rigidbody platformRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDirection = Vector3.left;
+        platformRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(moveDirection.x * speed, moveDirection.y * speed, moveDirection.z);
+        platformRigidbody.velocity = movement;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("TurnLeft"))
+        {
+           switch(moveDirection)
+            {
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.left):
+                    moveDirection = Vector3.up;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.up):
+                    moveDirection = Vector3.right;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.right):
+                    moveDirection = Vector3.down;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.down):
+                    moveDirection = Vector3.left;
+                    break;
+            }
+        }
+        else if (other.gameObject.CompareTag("TurnRight"))
+        {
+            switch (moveDirection)
+            {
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.left):
+                    moveDirection = Vector3.down;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.down):
+                    moveDirection = Vector3.right;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.right):
+                    moveDirection = Vector3.up;
+                    break;
+                case Vector3 moveDirection when moveDirection.Equals(Vector3.up):
+                    moveDirection = Vector3.left;
+                    break;
+            }
+        }
+    }
+}
