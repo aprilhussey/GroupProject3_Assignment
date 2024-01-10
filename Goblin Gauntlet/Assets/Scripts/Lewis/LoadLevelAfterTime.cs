0d86d0c08b2ee8bae0ef34c8f786f3b86f0d0e0f
LewisMil <102025099+LewisMil@users.noreply.github.com>
2024-01-10 13:12:58 +0000

    LoadingScreensScenesScript

---
+++ Goblin Gauntlet/Assets/Scripts/Lewis/LoadLevelAfterTime.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterTime : MonoBehaviour
+{
+
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
+    private float timeElapsed;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
